90f26552b6e1a34c32e77f2d2cf73d75d34054ca
NaamaBarIlan <barilan.naama@gmail.com>
2019-06-22 11:18:41 -0700

    Added data normalization to .sql

--- data/books.sql
+++ data/books.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS bookshelves;
 
 CREATE TABLE books (
   id SERIAL PRIMARY KEY,
@@ -10,8 +11,23 @@ CREATE TABLE books (
   bookshelf TEXT
 );
 
+CREATE TABLE bookshelves (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
 INSERT INTO books (author, title, isbn, image_url, description, bookshelf) 
 VALUES('Fred Grant','History of Seattle','ISBN_10', 'http://books.google.com/books/content?id=YK3yZNFmdbUC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api', 'Operating for 29 years, the Seattle...','Love this one!');
 
 INSERT INTO books (author, title, isbn, image_url, description, bookshelf) 
 VALUES('Mark Arnold','The Best of the Harveyville Fun Times','1477259864', 'http://books.google.com/books/content?id=vDllrEsYLAMC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api', 'A sampling of the best material...','Super fun times');
+
+INSERT INTO bookshelves(name) SELECT DISTINCT bookshelf FROM books;
+
+ALTER TABLE books ADD COLUMN bookshelf_id INT;
+
+UPDATE books SET bookshelf_id=shelf.id FROM (SELECT * FROM bookshelves) AS shelf WHERE books.bookshelf = shelf.name;
+
+ALTER TABLE books DROP COLUMN bookshelf;
+
+ALTER TABLE books ADD CONSTRAINT fk_bookshelves FOREIGN KEY (bookshelf_id) REFERENCES bookshelves(id);
